e08b9522e54902ab61f4ec4609109bd20b19a602
yukitoto <yukitoto_tabletennis@yahoo.co.jp>
2016-10-28 14:15:28 +0900

    modify zoomToRect logic and criteria of zoom in or out.

--- Classes/IDMZoomingScrollView.m
+++ Classes/IDMZoomingScrollView.m
@@ -288,7 +288,7 @@
 	[NSObject cancelPreviousPerformRequestsWithTarget:_photoBrowser];
 	
 	// Zoom
-	if (self.zoomScale == self.maximumZoomScale) {
+	if (self.zoomScale == self.maximumDoubleTapZoomScale) {
 		
 		// Zoom out
 		[self setZoomScale:self.minimumZoomScale animated:YES];
@@ -299,7 +299,7 @@
 		CGSize targetSize = CGSizeMake(self.frame.size.width / self.maximumDoubleTapZoomScale, self.frame.size.height / self.maximumDoubleTapZoomScale);
 		CGPoint targetPoint = CGPointMake(touchPoint.x - targetSize.width / 2, touchPoint.y - targetSize.height / 2);
 
-		[self zoomToRect:CGRectMake(touchPoint.x, touchPoint.y, 1, 1) animated:YES];
+		[self zoomToRect:CGRectMake(targetPoint.x, targetPoint.y, targetSize.width, targetSize.height) animated:YES];
 		
 	}
 	
